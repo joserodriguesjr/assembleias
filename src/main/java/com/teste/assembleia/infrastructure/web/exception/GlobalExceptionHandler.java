321dd4dc0aef8ea66b12d92cebe1ff6932a741b7
runiorr <joseluisrjunior@gmail.com>
2025-06-08 14:44:20 -0300

    refact: handling exception in a better way

--- src/main/java/com/teste/assembleia/infrastructure/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/teste/assembleia/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.teste.assembleia.infrastructure.web.exception;
 
+import com.teste.assembleia.domain.exception.AssociateAlreadyVotedException;
 import com.teste.assembleia.domain.exception.ResourceNotFoundException;
 import com.teste.assembleia.domain.exception.VotingSessionStillRunningException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -13,17 +15,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ProblemDetail handleResourceNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Recurso não Encontrado");
-//        problemDetail.setType(URI.create("https://seusite.com/errors/nao-encontrado"));
-
+    public ProblemDetail handleResourceNotFound(ResourceNotFoundException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setTitle("Recurso Não Encontrado");
+        problemDetail.setDetail(ex.getMessage());
         problemDetail.setProperty("timestamp", Instant.now());
 
         return problemDetail;
@@ -54,60 +56,43 @@ public class GlobalExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
         problemDetail.setTitle("Sessão de Votação em Andamento");
         problemDetail.setDetail("A votação ainda não foi encerrada. Tente novamente mais tarde.");
-
         problemDetail.setProperty("tempoRestante", formattedTime.toString());
         problemDetail.setProperty("horarioEncerramento", ex.getEndTime());
 
         return problemDetail;
     }
 
-    /**
-     * Handler que traduz violações de constraint do banco de dados em respostas de erro HTTP claras.
-     * Especificamente, ele identifica quando um associado tenta votar duas vezes.
-     */
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ProblemDetail handleDataIntegrityViolation(DataIntegrityViolationException ex) {
-        Throwable rootCause = ex.getMostSpecificCause();
+        Throwable rootCause = ex.getCause();
 
-        if (rootCause.getMessage() != null &&
-                rootCause.getLocalizedMessage().contains("uk_vote_session_associate")) {
-
-            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                    HttpStatus.CONFLICT,
-                    "Este associado já votou nesta pauta. O voto não foi computado novamente.");
-            problemDetail.setTitle("Voto Duplicado");
-            problemDetail.setProperty("timestamp", Instant.now());
-
-            return problemDetail;
+        if (rootCause instanceof ConstraintViolationException &&
+                Objects.equals(((ConstraintViolationException) rootCause).getConstraintName(), "uk_vote_session_associate")) {
+            AssociateAlreadyVotedException associateAlreadyVotedException = new AssociateAlreadyVotedException("Este associado já votou nesta pauta. O voto não foi computado novamente.");
+            return handleAssociateAlreadyVoted(associateAlreadyVotedException);
         }
 
         log.error("Ocorreu uma violação de integridade de dados não esperada.", ex);
+        return handleGenericException(ex);
+    }
 
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "Ocorreu um erro interno ao processar a requisição. Verifique os dados enviados.");
-        problemDetail.setTitle("Erro de Integridade de Dados");
+    @ExceptionHandler(AssociateAlreadyVotedException.class)
+    public ProblemDetail handleAssociateAlreadyVoted(AssociateAlreadyVotedException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        problemDetail.setTitle("Voto Duplicado");
+        problemDetail.setDetail(ex.getMessage());
         problemDetail.setProperty("timestamp", Instant.now());
 
         return problemDetail;
     }
 
-
-//@ExceptionHandler(BusinessException.class)
-//    public ProblemDetail handleRegraDeNegocio(BusinessException ex) {
-//        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
-//        problemDetail.setTitle("Conflito de Regra de Negócio");
-//        problemDetail.setType(URI.create("https://seusite.com/errors/regra-de-negocio"));
-//        problemDetail.setProperty("timestamp", Instant.now());
-//        return problemDetail;
-//    }
-
     @ExceptionHandler(Exception.class)
-    public ProblemDetail handleGenericException(Exception ex, HttpServletRequest request) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        problemDetail.setTitle("Erro inesperado no servidor");
-//        problemDetail.setType(URI.create("https://seusite.com/errors/regra-de-negocio"));
+    public ProblemDetail handleGenericException(Exception ex) {
+        log.error("Erro não tratado capturado pelo handler genérico.", ex);
 
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle("Erro Interno do Servidor");
+        problemDetail.setDetail("Ocorreu um erro inesperado no servidor.");
         problemDetail.setProperty("timestamp", Instant.now());
 
         return problemDetail;
